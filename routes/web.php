e3597b58ac4612fc4aa8816746816ded6901a540
Rrens <yusufrendi313@gmail.com>
2023-12-06 20:24:32 +0700

    redirect admin

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::redirect('/iki', 'admin/product');
+
 Route::get('', [HomeController::class, 'index'])->name('home');
 Route::get('catalogue', [HomeController::class, 'catalogues'])->name('catalogue');
 Route::get('cart', [HomeController::class, 'cart'])->name('cart');
